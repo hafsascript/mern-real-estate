bcdccdad39ee63f0a6873016fe9bf7329d468897
tryhafsops <hafsahill17@gmail.com>
2024-01-01 07:37:05 -0800

    created sign up UI and made changes in header UI, assigned index.css background

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-orange-200 shadow-md'>
+    <header className='bg-emerald-300 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
           <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-              <span className='text-red-900'>Hafsa</span>
-              <span className='text-black'>Estate</span>
+              <span className='text-black text-3xl mt-1'>Hafsa</span>
+              <span className='text-black text-xl mt-2'>Estate</span>
             </h1>
            </Link>
           <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -18,13 +18,13 @@ export default function Header() {
           </form>
           <ul className='flex gap-4' >
             <Link to='/'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+              <li className='hidden sm:inline text-slate-900 hover:underline'>Home</li>
             </Link>
             <Link to='/about'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+              <li className='hidden sm:inline text-slate-900 hover:underline'>About</li>
             </Link>
             <Link to='/sign-in'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+              <li className='hidden sm:inline text-slate-900 hover:underline'>Sign in</li>
             </Link>
           </ul>
         
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-4xl text-center  my-7 text-neutral-900 font-semibold '>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type=" text" placeholder='username'
+        className='border p-3 rounded-lg' id='username' />
+        <input type=" email" placeholder='email'
+        className='border p-3 rounded-lg' id='email' />
+        <input type=" password" placeholder='password'
+        className='border p-3 rounded-lg' id='password' />
+        <button className='bg-rose-400 text-white my-4 font-semibold p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Sign Up</button>
+        
+
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have An Account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-pink-600'>Sign In</span>
+        </Link>
+      </div>
+
+      
+    </div>
   )
 }
