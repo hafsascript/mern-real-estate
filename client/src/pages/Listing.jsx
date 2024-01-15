a2352bc6e7b76859a6bae7efba23504a53986cb2
tryhafsops <hafsahill17@gmail.com>
2024-01-14 16:43:00 -0800

    added copy link on the image slider

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -24,6 +24,7 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [contact, setContact] = useState(false);
+    const [copied, setCopied] = useState(false);
     const {currentUser} = useSelector((state) => state.user);
     useEffect(()=>{
         const fetchListing = async ()=>{
@@ -68,6 +69,25 @@ export default function Listing() {
                   </SwiperSlide>
                 ))}
               </Swiper>
+
+              <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
+
              
               <div className='flex flex-col max-w-4xl mx-auto p-3 my-5 gap-3'>
                 <p className='text-2xl font-semibold'>
