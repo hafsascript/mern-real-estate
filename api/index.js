e530649c57864e44c83b586ac6dca3ae1bbf6173
tryhafsops <hafsahill17@gmail.com>
2024-01-20 04:15:39 -0800

    backend changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+
 
 
 
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
 }).catch((err) => {console.log(err);});
 
-const __dirname = path.resolve();
 
 
 
@@ -30,11 +29,6 @@ app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
