01edada85ae6f8ee4926c06afecd9bd622a439c8
tryhafsops <hafsahill17@gmail.com>
2024-04-01 12:07:26 +0500

    deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://real-estate-api-gamma.vercel.app/',
-        secure: false,},
+        target: 'http://localhost:3000',
+        secure: false,
+      },
     },
   },
+
   plugins: [react()],
-})
+});
